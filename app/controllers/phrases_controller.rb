036e82da96ef54c5cc1bbcad9b4ed708ce9aa4bf
Mike <cznrhubarb@gmail.com>
2019-03-10 16:51:16 -0400

    Trying another thing with routing

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,25 +1,6 @@
 class PhrasesController < ApplicationController
-  def index
-    render plain: "Uh"
-  end
-  
-  def show
+
+  def random
     render plain: "OK"
-  end
-
-  def new
-  end
-
-  def create
-    render plain: "Create?"
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  resources :phrases
+  match '/phrases' => 'phrases#random', via: :post
 end
