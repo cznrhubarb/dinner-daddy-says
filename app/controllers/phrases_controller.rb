b5d96c0de980023a8a9ff27937a07165d20b73ac
Mike <cznrhubarb@gmail.com>
2019-03-14 13:47:33 -0400

    Adding in the ability to add phrases and randomly retrieve them

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -2,6 +2,19 @@ class PhrasesController < ApplicationController
   protect_from_forgery with: :null_session
   
   def random
-    render plain: "OK"
+    @random_phrase = Phrase.order("RANDOM()").first
+    @random_phrase.inspect
+    render plain: @random_phrase.text
+  end
+
+  def add  
+    @phrase = Phrase.new(phrase_params)
+
+    @phrase.save
+  end
+
+  private
+  def phrase_params
+    params.require(:phrase).permit(:text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   match '/phrases' => 'phrases#random', via: :post
+  match '/phrases/add' => 'phrases#add', via: :post
 end
