869e3302915f3f8dd73ce97926178fd5ea05adea
Mike <cznrhubarb@gmail.com>
2019-05-24 10:34:34 -0400

    Removing some extra stuff

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -1,16 +1,8 @@
-require 'httpclient'
-
 class PhrasesController < ApplicationController
-  protect_from_forgery with: :null_session
   
   def random
-    # return render json: {}, status: 403 unless valid_slack_token?
-    # PhraseWorker.perform_async(phrase_params.to_h)
-    # render json: { response_type: "in_channel" }, status: :created
-    # #client = HTTPClient.new
-    # #client.post(SLACK_API_ENDPOINT, params)
-    # ##render plain: @random_phrase.text
-    render json: { "response_type": "in_channel", "text": @random_phrase.text }
+    render plain: @random_phrase.text
+    #render json: { "response_type": "in_channel", "text": @random_phrase.text }
   end
 
   def add  
@@ -20,23 +12,7 @@ class PhrasesController < ApplicationController
   end
 
   private
-  def valid_slack_token?
-    params[:token] == ENV["SLACK_SLASH_COMMAND_TOKEN"]
-  end
-
-  def phrase_params
-    params.require(:phrase).permit(:text)
-  end
-
-  def post_params
-    @random_phrase = Phrase.order("RANDOM()").first
-
-    {
-      token: ENV['SLACK_OAUTH'],
-      channel: @to,
-      text: @random_phrase.text,
-      as_user: false,
-      link_names: true
-    }
-  end
+    def phrase_params
+      params.require(:phrase).permit(:text)
+    end
 end
