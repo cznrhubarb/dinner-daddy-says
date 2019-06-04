6487bb5c74544ac32dbea7f88560ab5f72ef895b
Mike <cznrhubarb@gmail.com>
2019-06-03 21:40:31 -0400

    Testing render plain with phrase params

--- app/controllers/phrases_controller.rb
+++ app/controllers/phrases_controller.rb
@@ -5,7 +5,19 @@ class PhrasesController < ApplicationController
     # TODO: Add an option to retrieve an indexed list of sayings
     # TODO: Add an option to pass in an index to get a specific saying
     
-    render plain: params[:text]
+    #render plain: params[:text]
+    if params[:text].upcase == "GOTEM"
+      everything = Phrase.all
+      everything.each do |p|
+        render plain: p.text
+      end
+      #render plain: "a list of entries"
+    elsif params[:text].to_i.to_s == params[:text]
+      render json: { "response_type": "in_channel", "text": Phrase.find(params[:text].to_i).text }
+    else
+      @random_phrase = Phrase.order("RANDOM()").first
+      render json: { "response_type": "in_channel", "text": @random_phrase.text }
+    end
     #@random_phrase = Phrase.order("RANDOM()").first
     #render json: { "response_type": "in_channel", "text": @random_phrase.text }
   end
